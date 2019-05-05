ee6926b958b78ba50bf77b51370dda1c668c2dbe
SpootDev <spootdev@gmail.com>
2019-05-05 15:05:34 -0500

    pop sqlite

--- source/web_app/mainapp/app.py
+++ source/web_app/mainapp/app.py
@@ -17,6 +17,7 @@ def create_app(config_object=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config_object)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     register_extensions(app)
     register_blueprints(app)
     register_errorhandlers(app)
