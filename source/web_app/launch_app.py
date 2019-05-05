1db180e30685670f038a0b3db5569e0c99ce24f5
SpootDev <spootdev@gmail.com>
2019-05-05 18:43:53 -0500

    what

--- source/web_app/launch_app.py
+++ source/web_app/launch_app.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from mainapp.database import db
 
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 
@@ -12,3 +13,12 @@ if BASE_DIR not in sys.path:
 from mainapp.app import create_app
 
 application = create_app()
+print('2hi')
+# if first user set it as administrator and create if not exists
+if os.path.isfile('/poestat/secure/db.sqlite'):
+    print('2hi2')
+    pass
+else:
+    print('2hi3')
+    db.create_all()
+    print('2hi4')
