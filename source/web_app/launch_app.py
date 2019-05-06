211b772cbae86bad4f66590db6ba35044c42f67a
SpootDev <spootdev@gmail.com>
2019-05-05 19:18:00 -0500

    purge from launch

--- source/web_app/launch_app.py
+++ source/web_app/launch_app.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-from mainapp.database import db
+# from mainapp.database import db
 
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 
@@ -14,11 +14,11 @@ from mainapp.app import create_app
 
 application = create_app()
 print('2hi')
-# if first user set it as administrator and create if not exists
-if os.path.isfile('/poestat/secure/db.sqlite'):
-    print('2hi2')
-    pass
-else:
-    print('2hi3')
-    db.create_all(application)
-    print('2hi4')
+# # if first user set it as administrator and create if not exists
+# if os.path.isfile('/poestat/secure/db.sqlite'):
+#     print('2hi2')
+#     pass
+# else:
+#     print('2hi3')
+#     db.create_all(application)
+#     print('2hi4')
