504ecea34401e5103fd66a985ccd8284ec907df4
SpootDev <spootdev@gmail.com>
2019-05-07 15:33:18 -0500

    missing row data

--- source/database/db_base_account.py
+++ source/database/db_base_account.py
@@ -50,15 +50,15 @@ def db_base_account_char_return(self):
     for row_data in self.db_cursor.fetchall():
         if row_data['poe_account_uuid'] in account_char_dict:
             print(row_data['poe_account_uuid'])
-            temp_list = account_char_dict['poe_account_uuid'][1]
+            temp_list = account_char_dict[row_data['poe_account_uuid']][1]
             print(temp_list)
             temp_list.append(row_data['db_poe_character_uuid'], row_data['db_poe_character_name'])
             print(temp_list)
-            account_char_dict['poe_account_uuid'] = (row_data['poe_account_name'], temp_list)
+            account_char_dict[row_data['poe_account_uuid']] = (row_data['poe_account_name'], temp_list)
             print(account_char_dict)
 
         else:
-            account_char_dict['poe_account_uuid'] = (row_data['poe_account_name'],
+            account_char_dict[row_data['poe_account_uuid']] = (row_data['poe_account_name'],
                                                      ((row_data['db_poe_character_uuid'],
                                                       row_data['db_poe_character_name'])))
     print(account_char_dict)
