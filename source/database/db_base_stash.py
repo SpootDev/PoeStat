f986f7b36c40781ba061e63f3184d8f3e420550a
SpootDev <spootdev@gmail.com>
2019-05-09 19:00:39 -0500

    db_poe_account_item_class_subtype_uuid

--- source/database/db_base_stash.py
+++ source/database/db_base_stash.py
@@ -78,7 +78,7 @@ def db_stash_items_by_account_count(self, account_uuid, base_uuid, subtype_uuid,
             # base uuid is set (helmet, gloves, etc)
             self.db_cursor.execute('select count(*) from db_poe_account_items'
                                    ' where db_poe_account_item_account_uuid = %s'
-                                   ' and db_poe_account_item_class_uuid'
+                                   ' and db_poe_account_item_class_subtype_uuid'
                                    ' in (select db_poe_item_subtype_class_uuid'
                                    ' from db_poe_item_class, db_poe_item_subtypes'
                                    ' where db_poe_item_subtype_class_uuid = db_poe_item_class_uuid'
@@ -88,7 +88,7 @@ def db_stash_items_by_account_count(self, account_uuid, base_uuid, subtype_uuid,
         # subtype is set (tornado wand, imperial claw, etc)
         self.db_cursor.execute('select count(*) from db_poe_account_items'
                                ' where db_poe_account_item_account_uuid = %s'
-                               ' and db_poe_account_item_class_uuid = %s',
+                               ' and db_poe_account_item_class_subtype_uuid = %s',
                                (account_uuid, subtype_uuid))
     return self.db_cursor.fetchone()[0]
 
@@ -108,7 +108,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
             self.db_cursor.execute('select db_poe_account_item_uuid, db_poe_account_item_json'
                                    ' from db_poe_account_items'
                                    ' where db_poe_account_item_account_uuid = %s'
-                                   ' and db_poe_account_item_class_uuid'
+                                   ' and db_poe_account_item_class_subtype_uuid'
                                    ' in (select db_poe_item_subtype_class_uuid'
                                    ' from db_poe_item_class, db_poe_item_subtypes'
                                    ' where db_poe_item_subtype_class_uuid = db_poe_item_class_uuid'
@@ -120,7 +120,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
         self.db_cursor.execute('select db_poe_account_item_uuid, db_poe_account_item_json'
                                ' from db_poe_account_items'
                                ' where db_poe_account_item_account_uuid = %s'
-                               ' and db_poe_account_item_class_uuid = %s',
+                               ' and db_poe_account_item_class_subtype_uuid = %s',
                                ' offset %s limit %s',
                                (account_uuid, subtype_uuid, offset, record_limit))
     return self.db_cursor.fetchall()
