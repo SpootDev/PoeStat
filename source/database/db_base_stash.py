c3492f6a5b04fc1f088eeb7de7df908570c453d7
SpootDev <spootdev@gmail.com>
2019-05-10 13:16:07 -0500

    order by names

--- source/database/db_base_stash.py
+++ source/database/db_base_stash.py
@@ -104,6 +104,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
             self.db_cursor.execute('select db_poe_account_item_uuid, db_poe_account_item_json'
                                    ' from db_poe_account_items'
                                    ' where db_poe_account_item_account_uuid = %s'
+                                   ' order by db_poe_item_class_name, db_poe_item_subtype_name'
                                    ' offset %s limit %s',
                                    (account_uuid, offset, record_limit))
         else:
@@ -116,6 +117,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
                                    ' from db_poe_item_class, db_poe_item_subtypes'
                                    ' where db_poe_item_subtype_class_uuid = db_poe_item_class_uuid'
                                    ' and db_poe_item_class_uuid = %s)'
+                                   ' order by db_poe_item_class_name, db_poe_item_subtype_name'
                                    ' offset %s limit %s',
                                    (account_uuid, base_uuid, offset, record_limit))
     else:
@@ -124,6 +126,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
                                ' from db_poe_account_items'
                                ' where db_poe_account_item_account_uuid = %s'
                                ' and db_poe_account_item_class_subtype_uuid = %s'
+                               ' order by db_poe_item_class_name, db_poe_item_subtype_name'
                                ' offset %s limit %s',
                                (account_uuid, subtype_uuid, offset, record_limit))
     return self.db_cursor.fetchall()
