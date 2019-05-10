411703806ec5cfb37e15cc3b9793a5f7c30b916d
SpootDev <spootdev@gmail.com>
2019-05-09 21:50:54 -0500

    db_poe_item_subtype_uuid

--- source/database/db_base_stash.py
+++ source/database/db_base_stash.py
@@ -80,7 +80,7 @@ def db_stash_items_by_account_count(self, account_uuid, base_uuid, subtype_uuid,
             self.db_cursor.execute('select count(*) from db_poe_account_items'
                                    ' where db_poe_account_item_account_uuid = %s'
                                    ' and db_poe_account_item_class_subtype_uuid'
-                                   ' in (select db_poe_item_subtype_class_uuid'
+                                   ' in (select db_poe_item_subtype_uuid'
                                    ' from db_poe_item_class, db_poe_item_subtypes'
                                    ' where db_poe_item_subtype_class_uuid = db_poe_item_class_uuid'
                                    ' and db_poe_item_class_uuid = %s)',
@@ -111,7 +111,7 @@ def db_stash_items_by_account(self, account_uuid, base_uuid, subtype_uuid, leagu
                                    ' from db_poe_account_items'
                                    ' where db_poe_account_item_account_uuid = %s'
                                    ' and db_poe_account_item_class_subtype_uuid'
-                                   ' in (select db_poe_item_subtype_class_uuid'
+                                   ' in (select db_poe_item_subtype_uuid'
                                    ' from db_poe_item_class, db_poe_item_subtypes'
                                    ' where db_poe_item_subtype_class_uuid = db_poe_item_class_uuid'
                                    ' and db_poe_item_class_uuid = %s)'
