f67140b61217026f4e408d30598d660623162863
SpootDev <spootdev@gmail.com>
2019-05-21 11:23:49 -0500

    remove link and set post

--- source/web_app/mainapp/user/views_stash.py
+++ source/web_app/mainapp/user/views_stash.py
@@ -109,6 +109,8 @@ def stash_item_list(base_uuid, subtype_uuid):
                                page=page,
                                per_page=per_page,
                                pagination=pagination,
+                               base_uuid=base_uuid,
+                               subtype_uuid=subtype_uuid
                                )
 
 
