bc85018012f85808666dae8fa1e157f2a13c493d
SpootDev <spootdev@gmail.com>
2019-05-20 19:28:48 -0500

    process filter

--- source/web_app/mainapp/user/views_stash.py
+++ source/web_app/mainapp/user/views_stash.py
@@ -13,6 +13,7 @@ sys.path.append('..')
 sys.path.append('../..')
 from common import common_config_ini
 from common import common_pagination
+from common import common_stash_search
 import database as database_base
 from mainapp.user.forms import StashSearchForm
 
@@ -43,37 +44,70 @@ def stash_item_list(base_uuid, subtype_uuid):
     # except AttributeError:
     #     print('hereiam2')
     #     return redirect(url_for('user.members'))
-    if request.method == 'POST':
-        shaper_item = request.form['search_form_shaper_item']
-        elder_item = request.form['search_form_elder_item']
-        socket_number = request.form('search_form_total_sockets')
-        armor_points = request.form('search_form_minimum_armor')
     # TODO use selected account
     g.account_uuid = g.db_connection.db_base_account_uuid_by_name('spooticusmaximus')
-    page, per_page, offset = common_pagination.get_page_items()
-    pagination = common_pagination.get_pagination(page=page,
-                                                  per_page=per_page,
-                                                  total=g.db_connection.db_stash_items_by_account_count(
-                                                      g.account_uuid,
-                                                      base_uuid,
-                                                      subtype_uuid,
-                                                      None),
-                                                  record_name='Stash Items',
-                                                  format_total=True,
-                                                  format_number=True,
-                                                  )
-    return render_template('users/user_account_stash.html',
-                           form=StashSearchForm(request.form),
-                           stash_items=g.db_connection.db_stash_items_by_account(g.account_uuid,
-                                                                                 base_uuid,
-                                                                                 subtype_uuid, None,
-                                                                                 offset, per_page),
-                           account_player=g.db_connection.db_base_account_char_return(),
-                           data_items=g.db_connection.db_item_base_item_tree(),
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination,
-                           )
+    if request.method == 'POST':
+        page, per_page, offset = common_pagination.get_page_items()
+        # populate the item list
+        stash_filter_items = common_stash_search.com_search_stash(
+            g.db_connection.db_stash_items_by_account(g.account_uuid, base_uuid, subtype_uuid, None, offset, per_page),
+            shaper_item=request.form['search_form_shaper_item'],
+            eldar_item=request.form['search_form_elder_item'],
+            veiled_item=False,
+            corrupted_item=False,
+            beastcraft_item=False,
+            fractured_item=False,
+            synthesized_item=False,
+            number_of_sockets=request.form('search_form_total_sockets'),
+            number_of_links=None,
+            armor_points=request.form('search_form_minimum_armor'),
+            es_points=0,
+            evasion_points=0, chance_to_block=0,
+            life_points=0, mana_points=0,
+            minimum_ilvl=0,
+            fire_rez=0, cold_rez=0,
+            light_rez=0, chaos_rez=0)
+        pagination = common_pagination.get_pagination(page=page,
+                                                      per_page=per_page,
+                                                      total=len(stash_filter_items),
+                                                      record_name='Stash Items',
+                                                      format_total=True,
+                                                      format_number=True,
+                                                      )
+        return render_template('users/user_account_stash.html',
+                               form=StashSearchForm(request.form),
+                               stash_items=stash_filter_items,
+                               account_player=g.db_connection.db_base_account_char_return(),
+                               data_items=g.db_connection.db_item_base_item_tree(),
+                               page=page,
+                               per_page=per_page,
+                               pagination=pagination,
+                               )
+    else:
+        page, per_page, offset = common_pagination.get_page_items()
+        pagination = common_pagination.get_pagination(page=page,
+                                                      per_page=per_page,
+                                                      total=g.db_connection.db_stash_items_by_account_count(
+                                                          g.account_uuid,
+                                                          base_uuid,
+                                                          subtype_uuid,
+                                                          None),
+                                                      record_name='Stash Items',
+                                                      format_total=True,
+                                                      format_number=True,
+                                                      )
+        return render_template('users/user_account_stash.html',
+                               form=StashSearchForm(request.form),
+                               stash_items=g.db_connection.db_stash_items_by_account(g.account_uuid,
+                                                                                     base_uuid,
+                                                                                     subtype_uuid, None,
+                                                                                     offset, per_page),
+                               account_player=g.db_connection.db_base_account_char_return(),
+                               data_items=g.db_connection.db_item_base_item_tree(),
+                               page=page,
+                               per_page=per_page,
+                               pagination=pagination,
+                               )
 
 
 @blueprint.before_request
