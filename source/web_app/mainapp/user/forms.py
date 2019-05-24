78105205a4e29956b2e344d7dc040d56fadbb904
SpootDev <spootdev@gmail.com>
2019-05-23 20:54:41 -0500

    min zero

--- source/web_app/mainapp/user/forms.py
+++ source/web_app/mainapp/user/forms.py
@@ -51,7 +51,7 @@ class StashSearchForm(Form):
                                                      (4, '4'),
                                                      (5, '5'),
                                                      (6, '6')])
-    search_form_minimum_armor = IntegerField('Min Armor', validators=[NumberRange(min=0, max=10000)])
+    search_form_minimum_armor = IntegerField('Min Armor', default=0, validators=[NumberRange(min=0, max=10000)])
 
     def __init__(self, *args, **kwargs):
         super(StashSearchForm, self).__init__(*args, **kwargs)
